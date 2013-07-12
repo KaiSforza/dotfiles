ad1d18b871026ace9ff74918e7e39bdee9878994
William Giokas <1007380@gmail.com>
2013-07-12 00:11:42 -0500

    dwm: added a new layout

--- config.dwm.h
+++ config.dwm.h
@@ -73,6 +73,7 @@ static const Layout layouts[] = {/*{{{*/
     { "| []- »",      tile },    /* first entry is default */
     { "| ><> »",      NULL },    /* no layout function means floating behavior */
     { "| [M] »",      monocle },
+    { "| -|- »",      tcl  },
 };/*}}}*/
 
 /* key definitions *//*{{{*/
@@ -130,6 +131,7 @@ static Key keys[] = {/*{{{*/
     { MODKEY,                       XK_e,      setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_w,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[4]} },
     { MODKEY,                       XK_r,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
