7b88d02e40c429de135d5afdb8aa9079b335aee2
William Giokas <1007380@gmail.com>
2014-08-31 15:10:46 -0500

    i3: Fix up some status things

--- i3/status.py
+++ i3/status.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#import subprocess
+# import subprocess
 
 from i3pystatus import Status
 
@@ -15,11 +15,13 @@ status.register("clock",
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
 status.register("load",
-                format="{avg1}:{avg5}:{avg15}")
+                format="{avg1}:{avg5}:{avg15}",
+                critical_limit=4)
 
 # Shows your CPU temperature, if you have a Intel CPU
 status.register("temp",
                 format="{temp:.0f}°C",)
+               # base_path="/sys/devices/platform/coretemp.0/hwmon/hwmon1")
 
 # The battery monitor has many formatting options, see README for details
 
