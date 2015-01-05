d1c8676c966fd30a2a487d34075e31d428e6a14c
William Giokas <1007380@gmail.com>
2015-01-04 22:39:02 -0600

    Add checks for if we're in tmux.
    For some reason the venv gets messed up in tmux. Probably a
    gnome-terminal thing.

--- zsh/20venv.zsh
+++ zsh/20venv.zsh
@@ -1,10 +1,12 @@
 # Load up the python 3 virtualenv if it exists. If not, try loading the
 # python 2 virtualenv. Otherwise we use the __CUSTOM_VENV environment
 # variable.
-if [[ -n "$__CUSTOM_VENV" && -f "$__CUSTOM_VENV/bin/activate" ]]; then
-  . "$__CUSTOM_VENV/bin/activate"
-elif [[ -f $HOME/.virtualenv/3/bin/activate ]]; then
-  . $HOME/.virtualenv/3/bin/activate
-elif [[ -f $HOME/.virtualenv/2/bin/activate ]]; then
-  . $HOME/.virtualenv/2/bin/activate
+if [[ -n $TMUX ]]; then
+    if [[ -n "$__CUSTOM_VENV" && -f "$__CUSTOM_VENV/bin/activate" ]]; then
+        . "$__CUSTOM_VENV/bin/activate"
+    elif [[ -f $HOME/.virtualenv/3/bin/activate ]]; then
+        . $HOME/.virtualenv/3/bin/activate
+    elif [[ -f $HOME/.virtualenv/2/bin/activate ]]; then
+        . $HOME/.virtualenv/2/bin/activate
+    fi
 fi
