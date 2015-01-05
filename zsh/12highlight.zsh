a098216a33576c18a6a39910ffb77318394c96be
William Giokas <1007380@gmail.com>
2015-01-04 22:38:19 -0600

    Fix colors on dark backgrounds

--- zsh/12highlight.zsh
+++ zsh/12highlight.zsh
@@ -11,7 +11,11 @@ ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=red'
 # make it look like vim, kind of
 ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=red'
 ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=red'
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=yellow'
 
 # Assignment of variables
 ZSH_HIGHLIGHT_STYLES[assign]='fg=red'
+
+# Blue looks awful on a black terminal most of the time.
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=cyan,underline'
